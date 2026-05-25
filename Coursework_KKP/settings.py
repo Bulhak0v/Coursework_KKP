5a90696be9c0cd5364ca9a8115d9a3a642c35c23
Bulhak0v <20knight06@gmail.com>
2026-05-25 19:05:56 +0300

    update setting.py static storages

--- Coursework_KKP/settings.py
+++ Coursework_KKP/settings.py
@@ -150,6 +150,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
